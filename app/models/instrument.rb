1bd98ed262f4ff613bd459986e019c61cb8386a2
philippe CHIRIE <p.chirie@laposte.net>
2019-11-18 18:13:25 +0100

    IntrumentUpdate

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,7 +2,6 @@ class Instrument < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
   validates :category, presence: true, inclusion: { in: ["vents", "cordes", "percussions"] }
