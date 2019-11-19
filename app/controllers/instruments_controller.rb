c1c9835ea7693944654476fa847436f99d9acb96
clement camara <clementcamara@gmail.com>
2019-11-19 14:42:54 +0100

    CRUD instrment

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -6,4 +6,39 @@ class InstrumentsController < ApplicationController
   def show
     @instrument = Instrument.find(params[:id])
   end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instruments_params)
+    # @user = User.find(params[:user_id])
+    # @instrument.user = @user
+    if @instrument.save
+      redirect_to "/"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def update
+    @instrument.update(instruments_params)
+    redirect_to instrument_path
+  end
+
+  def destroy
+    @instrument.destroy
+    redirect_to instruments_path
+  end
+
+  private
+
+  def instruments_params
+    params.require(:instrument).permit(:description, :unit_price, :category, :user_id)
+  end
 end
