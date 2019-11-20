7d58f3490a9dce90af1756a0fca832a93e78d079
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2019-11-20 16:06:36 +0100

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true, format: { with: /\A.*@.*\.*\z/ }, uniqueness: true
   validates :password, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
