35b5602d084a79744d50cf67c69fa8d1f0fb27c0
clement camara <clementcamara@gmail.com>
2019-11-20 17:41:35 +0100

    change migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_20_145853) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
@@ -46,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_145853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.boolean "owner", default: false
+    t.boolean "owner"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
