18d0d5a83753dff9b89501ece2591896441eaa5d
clement camara <clementcamara@gmail.com>
2019-11-22 12:30:19 +0100

    front image

--- db/schema.rb
+++ db/schema.rb
@@ -48,11 +48,10 @@ ActiveRecord::Schema.define(version: 2019_11_21_153609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.boolean "owner", default: false
+    t.boolean "owner"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "user"
     t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
